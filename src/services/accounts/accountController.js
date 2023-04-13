a44ef1bc96d3d18a637bb337eb9360bd3d818140
thomas-purcell <purcell.t@northeastern.edu>
2023-04-13 01:10:18 -0400

    another possible fix

--- src/services/accounts/accountController.js
+++ src/services/accounts/accountController.js
@@ -5,7 +5,7 @@ const cookieOptions = {
   expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // one week
   secure: true,
   httpOnly: true,
-  sameSite: 'Strict',
+  sameSite: 'none',
 };
 
 const registrationHandler = async (req, res) => {
